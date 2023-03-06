d98dcc98ee835b1ece7e021284cdf8bf977f9860
Harjas-Sidhu <harjassidhu1@gmail.com>
2023-03-07 01:12:51 +0530

    new file:   assets/images/cosplay.webp 	new file:   assets/images/merch.webp 	new file:   assets/images/quiz.webp 	modified:   assets/scripts/link.js 	new file:   assets/stylesheets/event_page_bar.css 	new file:   assets/stylesheets/event_page_contents.css 	modified:   assets/stylesheets/header.css 	renamed:    assets/stylesheets/bar.css -> assets/stylesheets/homepage_bar.css 	renamed:    assets/stylesheets/bar_content.css -> assets/stylesheets/homepage_bar_content.css 	modified:   assets/stylesheets/index.css 	modified:   index.html

--- assets/scripts/link.js
+++ assets/scripts/link.js
@@ -1,3 +1,16 @@
-const link = (address) => {
-    window.location.href = address;
+const link = (page_id) => {
+    let pages = document.getElementsByClassName('event_page');
+    for (let i = 0; i < pages.length; i++) {
+        pages[i].style.display = 'none';
+    }
+    document.getElementById('homepage').style.display = 'none';
+    if(page_id != 'homepage'){
+        document.getElementById('arrow').style.display = 'flex';
+    }
+    else{
+        document.getElementById('arrow').style.display = 'none';
+    }
+    document.getElementById(page_id).style.display = 'flex';
 }
+
+link('homepage');
