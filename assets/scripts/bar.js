cae8c3297ddd551793b14ee277f1ec380005d38d
Harjas-Sidhu <harjassidhu1@gmail.com>
2023-03-25 16:14:57 +0530

    modified:   assets/scripts/bar.js 	modified:   assets/stylesheets/homepage_bar.css

--- assets/scripts/bar.js
+++ assets/scripts/bar.js
@@ -6,7 +6,7 @@ dates_item_content = document.getElementsByClassName('dates_item_content');
 const tabs = (dates_item_content_number) => {
     for (let i = 0; i < dates_item_content.length; i++) {
         dates_item_content[i].style.display = 'none';
-        dates_item[i].style.color = '#343643';
+        dates_item[i].style.color = '#565656';
         dates_item[i].style.borderBottom = '0';
     }
     document.getElementById(dates_item_content_number).style.display = 'flex';
