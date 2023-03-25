dba9bfdafc1be1359ec77d56da8d4e87bc54fcad
Harjas-Sidhu <harjassidhu1@gmail.com>
2023-03-25 15:25:01 +0530

    modified:   assets/scripts/sidebar.js

--- assets/scripts/sidebar.js
+++ assets/scripts/sidebar.js
@@ -14,7 +14,7 @@ const OpenNav = () => {
     setTimeout(() => {
         document.getElementById('side_bar').style.backdropFilter = 'blur(10px)';
         document.getElementById('side_bar').style['-webkit-backdrop-filter'] = 'blur(10px)';
-    }, 300);
+    }, 800);
     nav = true;
 }
 
