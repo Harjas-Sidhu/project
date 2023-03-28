f6ebbeaf4986b4c5269f04917c8b9ff59ee2bb25
Harjas-Sidhu <harjassidhu1@gmail.com>
2023-03-28 21:26:54 +0530

    modified:   assets/scripts/preloader.js 	modified:   assets/stylesheets/other_basic_page.css

--- assets/scripts/preloader.js
+++ assets/scripts/preloader.js
@@ -1,5 +1,7 @@
 window.onload = () => {
+    document.getElementsByTagName('body')[0].style.overflow = 'hidden';
     setTimeout(() => {
         document.getElementById('preloader').style.display = 'none';
+        document.getElementsByTagName('body')[0].style.overflowY = 'scroll';
     }, 2000);
 };
