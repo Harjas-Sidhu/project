00268bcc0a2d35c84d8dacbf041c9ac72b2c2b42
Harjas-Sidhu <harjassidhu1@gmail.com>
2023-06-14 17:27:17 +0530

    modified:   assets/scripts/preloader.js

--- assets/scripts/preloader.js
+++ assets/scripts/preloader.js
@@ -3,8 +3,10 @@ window.onload = () => {
     setTimeout(() => {
         document.getElementById('preloader').style.display = 'none';
         document.getElementsByTagName('body')[0].style.overflowY = 'scroll';
-        document.getElementsByClassName('discirpt-box')[0].style.animation = 'opening 1.5s ease-in-out';
-        document.getElementsByClassName('heading')[0].style.animation = 'Aryan 2s linear';
-        document.getElementsByClassName('para')[0].style.animation = 'Aryan-para 2s linear';
+        if(document.getElementsByClassName('discirpt-box')[0]){
+            document.getElementsByClassName('heading')[0].style.animation = 'Aryan 2s linear';
+            document.getElementsByClassName('para')[0].style.animation = 'Aryan-para 2s linear';
+            document.getElementsByClassName('discirpt-box')[0].style.animation = 'opening 1.5s ease-in-out';
+        }
     }, 2000);
 };
